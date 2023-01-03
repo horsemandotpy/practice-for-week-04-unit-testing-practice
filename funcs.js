15f85160134f81d2bac18a164e470f1cc761da15
vupn <vupham140199@gmail.com>
2023-01-03 08:02:41 +0700

    Completed

--- funcs.js
+++ funcs.js
@@ -1,14 +1,44 @@
 function isFive(num) {
   // Your code here
+  if (num === 5) {
+    return true;
+  }
+  return false;
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number === "number") {
+    if (number % 2 !== 0) {
+      return true;
+    }
+
+    return false;
+  } else {
+    throw new TypeError("not a number");
+  }
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
-}
+  // Edge case: input is not number
+  // Unintuitive case: bigger number first
+  // Take in a min, a max and a step
+  // If step is not provided, default 1
+  // Make empty array holder
+  // For number from min to max
+  // Add eachone to array skip by step
+  // Return Array
+  //
+  if (min >= max) {
+    return [];
+  }
 
+  const yourRange = [];
+  for (let i = min; i <= max; i += step) {
+    yourRange.push(i);
+  }
+  return yourRange;
+}
 
 module.exports = { isFive, isOdd, myRange };
--- test/class-spec.js
+++ test/class-spec.js
@@ -3,32 +3,38 @@ const expect = chai.expect;
 
 const { Word } = require("../class");
 
-describe("Word", function () {
-  describe("Word constructor function", function () {
-    it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+describe("Word", function() {
+  let word;
+
+  beforeEach(() => {
+    word = new Word("word");
+  });
+
+  describe("Word constructor function", function() {
+    it('should have a "word" property', function() {
+      expect(word.word).to.exist;
     });
-  
-    it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+
+    it('should set the "word" property when a new word is created', function() {
+      expect(word.word).to.equal("word");
     });
   });
 
-  describe("removeVowels function", function () {
-    it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+  describe("removeVowels function", function() {
+    it("should return a the word with all vowels removed", function() {
+      expect(word.removeVowels()).to.equal("wrd");
     });
   });
 
-  describe("removeConsonants function", function () {
-    it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+  describe("removeConsonants function", function() {
+    it("should return the word with the consonants removed", function() {
+      expect(word.removeConsonants()).to.equal("o");
     });
   });
-  
-  describe("pigLatin function", function () {
-    it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+
+  describe("pigLatin function", function() {
+    it("should return the word converted to pig latin", function() {
+      expect(word.pigLatin()).to.equal("ordway");
     });
   });
 });
